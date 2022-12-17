fd58bddeebd909d6bd0a427d923605c39a7c76e8
Joel Wood <joelmwood@live.com>
2022-12-17 07:55:13 -0600

    Added basic color change to sketch board

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const grid = document.querySelector(".gridContainer");
+const resetButton = document.querySelector(".reset");
+let color = "white";
 
 createGrid = () => {
-  for(let i=0; i < 256; i++){
-    const div = document.createElement("div");
-    div.classList.add("square");
-    grid.appendChild(div);
+  for (let i = 0; i < 256; i++) {
+    const divElement = document.createElement("div");
+    divElement.classList.add("square");
+    divElement.addEventListener('mouseover', changeColor)
+    grid.appendChild(divElement);
   }
 };
+
+function changeColor(e){
+  e.target.style.backgroundColor = "grey";
+}
+
+
+resetButton.addEventListener("click", function() {
+  grid.innerHTML = "";
+  grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
+  grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+  createGrid();
+});
 
 createGrid();
