bb0b82de2c58c7631cd45164458cd4988546d9f7
Joel Wood <joelmwood@live.com>
2022-12-19 07:20:49 -0600

    Fixed bug that sometimes caused mouseDown to stay down by adding user select none to CSS for grid

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+
 
 const grid = document.querySelector(".gridContainer");
 const resetButton = document.querySelector(".reset");
 const blackButton = document.querySelector(".black");
 const rainbowButton = document.querySelector(".rainbow");
-// const userInput = document.getElementById("gridSize");
 
 const sizeValue = document.getElementById("sizeValue");
 const sizeSlider = document.getElementById("sizeSlider");
@@ -20,6 +17,10 @@ let currentSize = DEFAULT_SIZE;
 const DEFAULT_COLOR = "black";
 let currentColorMode = DEFAULT_COLOR;
 
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 function updateSizeValue(value){
   sizeValue.innerHTML = `${value} x ${value}`;
@@ -44,39 +45,25 @@ function createGrid(size) {
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
   for (let i = 0; i < size * size; i++) {
-    const divElement = document.createElement("div");
-    // divElement.style.backgroundColor = "white";
-    divElement.classList.add("square");
-    divElement.addEventListener('mouseover', changeColor);
-    divElement.addEventListener('mousedown', changeColor);
-    grid.appendChild(divElement);
+    const gridElement = document.createElement("div");
+    gridElement.classList.add("square");
+    gridElement.addEventListener('mouseover', changeColor);
+    gridElement.addEventListener('mousedown', changeColor);
+    grid.appendChild(gridElement);
   }
 };
 
-// function updateGrid(){
-//   grid.innerHTML = "";
-//   if(userInput.value < 0 ){
-//     createGrid(2);
-//   }else if(userInput.value > 50){
-//     createGrid(50);
-//   }else {
-//     createGrid(userInput.value);
-//   }
-// }
-
-// userInput.addEventListener("change", updateGrid);
 
 function changeColor(e){
-  if(mouseDown){
-    if(currentColorMode === "black"){
-      e.target.style.backgroundColor = "black";
-    } else if(currentColorMode === "rainbow"){
-        const randomR = Math.floor(Math.random() * 256);
-        const randomG = Math.floor(Math.random() * 256);
-        const randomB = Math.floor(Math.random() * 256);
-        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG},${randomB})`
-    }       
-  }    
+  if (e.type === 'mouseover' && !mouseDown) return;
+  if(currentColorMode === `rainbow`){
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG},${randomB})`
+  } else if (currentColorMode === `black`){
+    e.target.style.backgroundColor = "black";
+  }
 }
 
 
@@ -95,4 +82,6 @@ rainbowButton.addEventListener("click", function() {
   currentColorMode = "rainbow";
 });
 
-createGrid(currentSize);
+window.onload = () => {
+  createGrid(currentSize);
+}
